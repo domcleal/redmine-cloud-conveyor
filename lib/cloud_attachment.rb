ce926f4a3e48e9b53bef1207976547c65d2235ce
Unknown <nathan@cellsixtyone.com>
2010-06-12 08:52:30 -0700

    Added copyright information to relevant files, and updated the README.rdoc to be more complete, and to match what's available on the wiki at https://bitbucket.org/nathan_aschbacher/redmine-cloud-conveyor/wiki/Home

--- lib/cloud_attachment.rb
+++ lib/cloud_attachment.rb
@@ -1,12 +1,28 @@
+# Cloud Conveyor - Cloud Files storage plugin for Redmine
+# Copyright (C) 2010  Nathan Aschbacher - Cell Sixty-One
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 module CloudConveyor
   module CloudAttachment
-    def self.included(base) # :nodoc:
+    def self.included(base)
       base.extend(ClassMethods)
       base.send(:include, InstanceMethods)
           
-      # Same as typing in the class 
       base.class_eval do
-        unloadable # Send unloadable so it will not be unloaded in development
+        unloadable 
 
         def before_save
           if @temp_file && (@temp_file.size > 0)
